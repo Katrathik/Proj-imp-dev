6d6b6ad3e5c6cc28ca1494a97b5b7ba17b7d6c32
Karthik <karthikkh9@gmail.com>
2024-08-08 14:56:53 +0530

    Saved

--- passOp/src/App.jsx
+++ passOp/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <Navbar/>
       <div className='bg-blue-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
       <Manager/>
-      <div>hello</div>
       </div>
       <Footer/>
     </>
