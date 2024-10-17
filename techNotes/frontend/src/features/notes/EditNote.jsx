74ad9bf9e5de01645bbf0f45e90f4c3f5341642c
Katrathik <127038616+Katrathik@users.noreply.github.com>
2024-10-17 18:09:26 +0530

    Update EditNote.jsx

--- techNotes/frontend/src/features/notes/EditNote.jsx
+++ techNotes/frontend/src/features/notes/EditNote.jsx
@@ -1,18 +1,42 @@
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectNoteById } from './notesApiSlice'
-import { selectAllUsers } from '../users/usersApiSlice'
 import EditNoteForm from './EditNoteForm'
+import { useGetNotesQuery } from './notesApiSlice'
+import { useGetUsersQuery } from '../users/usersApiSlice'
+import useAuth from '../../hooks/useAuth'
+import useTitle from '../../hooks/useTitle'
 
 const EditNote = () => {
+    useTitle('techNotes: Edit Note')
+
     const { id } = useParams()
 
-    // select note by id and get all users
-    const note = useSelector(state => selectNoteById(state, id))
-    const users = useSelector(selectAllUsers)
+    const { username, isManager, isAdmin } = useAuth()
 
-    // if we have note and users , render the noteform with users to choose form, else show loading
-    const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
+    const { note } = useGetNotesQuery("notesList", {
+        selectFromResult: ({ data }) => ({
+            // extract note by id matching the response
+            note: data?.entities[id]
+        }),
+    })
+
+    const { users } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            // extract all users from the api 
+            users: data?.ids.map(id => data?.entities[id])
+        }),
+    })
+
+    if (!note || !users?.length) return <p>Not Found!</p>
+
+
+    if (!isManager && !isAdmin) {
+        // if user is trying to edit any note not assigned to him via url, no access
+        if (note.username !== username) {
+            return <p className="errmsg">No access</p>
+        }
+    }
+
+    const content = <EditNoteForm note={note} users={users} />
 
     return content
 }
